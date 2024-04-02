8bd99e322230a8ece25b1cf1f8cfbcb460ae9911
KusokGoracio <lawrentiev.roma@gamil.com>
2024-04-02 21:51:49 +0700

    Добавьте файлы проекта.

---
+++ Lab7/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace Lab7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea13 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend13 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series19 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series20 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea14 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend14 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series21 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            this.priceNum = new System.Windows.Forms.NumericUpDown();
+            this.populationNum = new System.Windows.Forms.NumericUpDown();
+            this.initialPriceLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label2 = new System.Windows.Forms.Label();
+            this.InfLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.priceNum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.populationNum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(1219, 41);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 9;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(1219, 12);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 8;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click_1);
+            // 
+            // priceNum
+            // 
+            this.priceNum.DecimalPlaces = 2;
+            this.priceNum.Location = new System.Drawing.Point(90, 33);
+            this.priceNum.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.priceNum.Name = "priceNum";
+            this.priceNum.Size = new System.Drawing.Size(120, 20);
+            this.priceNum.TabIndex = 7;
+            this.priceNum.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // populationNum
+            // 
+            this.populationNum.Location = new System.Drawing.Point(90, 7);
+            this.populationNum.Maximum = new decimal(new int[] {
+            10000000,
+            0,
+            0,
+            0});
+            this.populationNum.Name = "populationNum";
+            this.populationNum.Size = new System.Drawing.Size(120, 20);
+            this.populationNum.TabIndex = 6;
+            this.populationNum.Value = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            // 
+            // initialPriceLabel
+            // 
+            this.initialPriceLabel.AutoSize = true;
+            this.initialPriceLabel.Location = new System.Drawing.Point(12, 14);
+            this.initialPriceLabel.Name = "initialPriceLabel";
+            this.initialPriceLabel.Size = new System.Drawing.Size(66, 13);
+            this.initialPriceLabel.TabIndex = 5;
+            this.initialPriceLabel.Text = "Население:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Цена мойки:";
+            // 
+            // chart1
+            // 
+            chartArea13.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea13);
+            legend13.Name = "Legend1";
+            this.chart1.Legends.Add(legend13);
+            this.chart1.Location = new System.Drawing.Point(15, 91);
+            this.chart1.Name = "chart1";
+            series19.BorderWidth = 3;
+            series19.ChartArea = "ChartArea1";
+            series19.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series19.Legend = "Legend1";
+            series19.Name = "Цена мойки";
+            series20.BorderWidth = 3;
+            series20.ChartArea = "ChartArea1";
+            series20.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series20.Legend = "Legend1";
+            series20.Name = "Кол. клиентов за месяц";
+            this.chart1.Series.Add(series19);
+            this.chart1.Series.Add(series20);
+            this.chart1.Size = new System.Drawing.Size(735, 476);
+            this.chart1.TabIndex = 11;
+            this.chart1.Text = "chart1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(230, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Кол. клиентов за месяц:";
+            // 
+            // InfLabel
+            // 
+            this.InfLabel.AutoSize = true;
+            this.InfLabel.Location = new System.Drawing.Point(368, 9);
+            this.InfLabel.Name = "InfLabel";
+            this.InfLabel.Size = new System.Drawing.Size(19, 13);
+            this.InfLabel.TabIndex = 13;
+            this.InfLabel.Text = "10";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // chart2
+            // 
+            chartArea14.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea14);
+            legend14.Name = "Legend1";
+            this.chart2.Legends.Add(legend14);
+            this.chart2.Location = new System.Drawing.Point(765, 91);
+            this.chart2.Name = "chart2";
+            series21.BorderWidth = 3;
+            series21.ChartArea = "ChartArea1";
+            series21.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series21.Color = System.Drawing.Color.Red;
+            series21.Legend = "Legend1";
+            series21.Name = "Население";
+            this.chart2.Series.Add(series21);
+            this.chart2.Size = new System.Drawing.Size(544, 476);
+            this.chart2.TabIndex = 14;
+            this.chart2.Text = "chart2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1321, 650);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.InfLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.priceNum);
+            this.Controls.Add(this.populationNum);
+            this.Controls.Add(this.initialPriceLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.priceNum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.populationNum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.NumericUpDown priceNum;
+        private System.Windows.Forms.NumericUpDown populationNum;
+        private System.Windows.Forms.Label initialPriceLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label InfLabel;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+    }
+}
+
---
+++ Lab7/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace Lab7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        const double priceCoef = 0.002, competitorsCoef = 0.00003, staffCoef = 0.0001, speedCoef = 0.5;
+
+        Random rnd = new Random();
+
+        double price, profit, serviceSpeed;
+
+        int population, competitors, staff, clientPerMonth;
+
+        int month;
+
+        double prevProfit;
+
+        private void startButton_Click_1(object sender, EventArgs e)
+        {
+            price = (double)priceNum.Value;
+            population = (int)populationNum.Value;
+            clientPerMonth = 10;
+
+            month = 0;
+            prevProfit = 10;
+            competitors = (int)(population * competitorsCoef);
+            staff = (int)(population * staffCoef);
+            serviceSpeed = staff * speedCoef;
+
+            chart2.Series[0].Points.Clear();
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+
+            chart2.Series[0].Points.AddXY(0, population);
+            chart1.Series[0].Points.AddXY(0, price);
+            chart1.Series[1].Points.AddXY(0, clientPerMonth);
+
+            timer1.Start();
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            month++;
+
+            profit = clientPerMonth * price;
+            price = prevProfit > profit ? price -= price * priceCoef : price += price * priceCoef;
+
+            competitors += (int)(population * competitorsCoef * (rnd.NextDouble() - 0.5));
+            staff += (int)(population * staffCoef * (rnd.NextDouble() - 0.5));
+            serviceSpeed = staff * speedCoef;
+
+            clientPerMonth += (int)(((price * 0.02) - (competitors * 0.4) + (serviceSpeed * 0.3)) * (rnd.NextDouble() - 0.45));
+
+            clientPerMonth = clientPerMonth > 0 ? clientPerMonth : 0;
+
+            prevProfit = profit;
+            population += (int)((rnd.NextDouble() - 0.49) * population * 0.1);
+
+            chart2.Series[0].Points.AddXY(month, population);
+            chart1.Series[0].Points.AddXY(month, price);
+            chart1.Series[1].Points.AddXY(month, clientPerMonth);
+
+            populationNum.Value = (decimal)population;
+            priceNum.Value = (decimal)price;
+            InfLabel.Text = Convert.ToString(clientPerMonth);
+        }
+    }
+}
